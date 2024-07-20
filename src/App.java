48dcfbe7664c052dcde0f0969d758f59990a642a
jkrussel <juddrussel.kalalo@gmail.com>
2024-07-20 11:35:12 +0200

    refactored App.java

--- src/App.java
+++ src/App.java
@@ -1,21 +1,18 @@
 /**
  *  @file App.java 
+ *  @author Judd Russel Kalalo
  * 
  * Questa è la classe che viene invocata per prima, poiché contiene il metodo main.
- * Il metodo istanzia gli oggetti di tipo Controller, Model, View per connettere l'interfaccia grafica
- * alla logica del programma, permettendone la comunicazione.
+ * Il metodo istanzia gli oggetti di tipo Controller, Database, View per connettere l'interfaccia grafica
+ * alla logica del programma, permettendone l'interazione.
  */
 
 import controller.Controller;
 import view.View;
 import model.Database;
-import model.esami.*;
 
 public class App {
 	public static void main(String args[]) {
-		
-		// Linking tra controller e view.
-		// In view sono definiti table e tableModel, mentre in controller è definito database. 
 		Database database = new Database();
 		Controller controller = new Controller();
 		controller.setDatabase(database);
@@ -23,7 +20,6 @@ public class App {
 		View view = new View(controller);
 		controller.setView(view);
 		
-		// Mostra l'interfaccia grafica all'utente
 		view.setVisible(true);
 	}
 }
