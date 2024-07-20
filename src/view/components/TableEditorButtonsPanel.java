b4bcfa7eaa178f60c30eca76b0ea8890a90590c4
jkrussel <juddrussel.kalalo@gmail.com>
2024-07-20 22:31:10 +0200

    removed unused imports

--- src/view/components/TableEditorButtonsPanel.java
+++ src/view/components/TableEditorButtonsPanel.java
@@ -1,7 +1,6 @@
 package view.components;
 
 import java.awt.Color;
-import java.awt.Dialog.ModalityType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -9,7 +8,6 @@ import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
