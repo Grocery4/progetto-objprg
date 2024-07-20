bc413b69fe2294937772304beda3cb67bc04f1ea
jkrussel <juddrussel.kalalo@gmail.com>
2024-07-20 22:31:47 +0200

    smalls scripts to facilitate life

---
+++ compile.bash
@@ -0,0 +1,5 @@
+
+# -d: specifica dove piazzare i file .class generati dalla compilazione
+# -cp specifica dove trovare le dipendenze del programma (.jar)
+# <A.java> <B.java>.. : specifica quali sono i file target della compilazione
+javac -d ./bin -cp ./src/lib/*:. src/App.java src/controller/Controller.java src/view/View.java src/view/components/*.java src/view/errors/*.java src/model/*.java src/model/esami/*.java
